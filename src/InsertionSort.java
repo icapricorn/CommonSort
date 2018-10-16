f2d12a950c8752ea3d26202992d09444c4de7355
icapricorn <icapricorn@yeah.net>
2018-10-16 10:43:33 +0800

    first commit

---
+++ src/InsertionSort.java
@@ -0,0 +1,11 @@
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+		sort();
+	}
+
+	private static void sort() {
+
+	}
+}
---
+++ src/SelectionSort.java
@@ -0,0 +1,11 @@
+
+public class SelectionSort {
+
+	public static void main(String[] args) {
+		sort();
+	}
+
+	private static void sort() {
+
+	}
+}
