8bb3d4a8b9b6c6397e0bab225805e29d253a7b49
icapricorn <icapricorn@yeah.net>
2018-10-17 21:36:42 +0800

    fullfill code

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,11 +1,20 @@
+import java.util.Arrays;
 
 public class InsertionSort {
 
-	public static void main(String[] args) {
-		sort();
-	}
+    public static void main(String[] args) {
+        Integer[] arr = {1, 8, 6, 7, 5, 4, 3, 2, 9};
+        sort(arr);
+        System.out.print(Arrays.toString(arr));
+    }
 
-	private static void sort() {
-
-	}
+    private static void sort(Comparable[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0 && arr[j].compareTo(j - 1) < 0; j--) {
+                Comparable tmp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = tmp;
+            }
+        }
+    }
 }
--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,11 +1,26 @@
+import java.util.Arrays;
 
 public class SelectionSort {
 
-	public static void main(String[] args) {
-		sort();
-	}
+    public static void main(String[] args) {
+        Integer[] arr = {1, 8, 6, 7, 5, 4, 3, 2, 9};
+        sort(arr);
+        System.out.print(Arrays.toString(arr));
+    }
 
-	private static void sort() {
-
-	}
+    private static void sort(Comparable[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int index = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j].compareTo(arr[index]) < 0) {
+                    index = j;
+                }
+            }
+            if (index != i) {
+                Comparable tmp = arr[i];
+                arr[i] = arr[index];
+                arr[index] = tmp;
+            }
+        }
+    }
 }
